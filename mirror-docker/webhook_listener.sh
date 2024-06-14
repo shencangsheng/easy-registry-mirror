cf0b0df53da05da8f4bd194361003dd430cfa9e8
liu.shiyu <shencangsheng@126.com>
2024-06-14 20:36:00 +0800

    feat: mirror

--- mirror-docker/webhook_listener.sh
+++ mirror-docker/webhook_listener.sh
@@ -13,29 +13,34 @@ while true; do
     BODY=$(awk 'NR>1{print}' request.log)
     echo "Received Body: $BODY"
 
-    IMAGE_NAME=$(echo "$BODY" | jq -r '.events[0].target.repository')
-    IMAGE_TAG=$(echo "$BODY" | jq -r '.events[0].target.tag // "latest"')
+    EVENT_ACTION=$(echo "$BODY" | jq -r '.events[0].action')
 
-    echo "Image pulled: ${IMAGE_NAME}:${IMAGE_TAG}"
+    if [ "$EVENT_ACTION" == "pull" ]; then
 
-    if [ -S "$SOCK" ]; then
-        echo "{\"fromImage\": \"${IMAGE_NAME}\", \"tag\": \"${IMAGE_TAG}\"}" |
-            curl --unix-socket /var/run/docker.sock -X POST -H "Content-Type: application/json" -d @- http:/v1.24/images/create?fromImage=${IMAGE_NAME} &
-        tag=${IMAGE_TAG}
+        IMAGE_NAME=$(echo "$BODY" | jq -r '.events[0].target.repository')
+        IMAGE_TAG=$(echo "$BODY" | jq -r '.events[0].target.tag // "latest"')
 
-        echo "Pulling image ${IMAGE_NAME}:${IMAGE_TAG} completed."
+        echo "Image pulled: ${IMAGE_NAME}:${IMAGE_TAG}"
 
-        NEW_IMAGE="${TARGET_REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}"
-        echo "{\"fromImage\": \"${IMAGE_NAME}\", \"repo\": \"${NEW_IMAGE}\"}" |
-            curl --unix-socket /var/run/docker.sock -X POST -H "Content-Type: application/json" -d @- http:/v1.24/images/${IMAGE_NAME}:${IMAGE_TAG}/tag?repo=${NEW_IMAGE}
+        if [ -S "$SOCK" ]; then
+            echo "{\"fromImage\": \"${IMAGE_NAME}\", \"tag\": \"${IMAGE_TAG}\"}" |
+                curl --unix-socket /var/run/docker.sock -X POST -H "Content-Type: application/json" -d @- http:/v1.24/images/create?fromImage=${IMAGE_NAME} &
+            tag=${IMAGE_TAG}
 
-        echo "Tagging image as ${NEW_IMAGE} completed."
+            echo "Pulling image ${IMAGE_NAME}:${IMAGE_TAG} completed."
 
-        echo "{\"tag\": \"${NEW_IMAGE}\"}" |
-            curl --unix-socket /var/run/docker.sock -X POST -H "Content-Type: application/json" -d @- http:/v1.24/images/${NEW_IMAGE}/push
+            NEW_IMAGE="${TARGET_REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}"
+            echo "{\"fromImage\": \"${IMAGE_NAME}\", \"repo\": \"${NEW_IMAGE}\"}" |
+                curl --unix-socket /var/run/docker.sock -X POST -H "Content-Type: application/json" -d @- http:/v1.24/images/${IMAGE_NAME}:${IMAGE_TAG}/tag?repo=${NEW_IMAGE}
 
-        echo "Pushing image ${NEW_IMAGE} completed."
-    else
-        echo "Docker socket not found at ${SOCK}"
+            echo "Tagging image as ${NEW_IMAGE} completed."
+
+            echo "{\"tag\": \"${NEW_IMAGE}\"}" |
+                curl --unix-socket /var/run/docker.sock -X POST -H "Content-Type: application/json" -d @- http:/v1.24/images/${NEW_IMAGE}/push
+
+            echo "Pushing image ${NEW_IMAGE} completed."
+        else
+            echo "Docker socket not found at ${SOCK}"
+        fi
     fi
 done
